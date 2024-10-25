fd75bd3ef98618c18932b3ade18956d41bc7b1ca
lauberlab <86684572+lauberlab@users.noreply.github.com>
2024-10-25 14:19:23 +0200

    Update R script

--- meta_analysis/metaAnalysis.r
+++ meta_analysis/metaAnalysis.r
@@ -2,16 +2,16 @@
 library(meta)
 
 data <- data.frame(
-  comparison = c("IRIS1 vs LoewenKIDS", "IRIS1+IRIS2 vs LoewenKIDS", "IRIS1+IRIS2+IRIS3 vs LoewenKIDS"),
-  OR = c(2.13158, 2.63903, 2.01582),      # odds ratios of Fisher-exact test
-  p_value = c(0.1970, 0.02726,0.0802 ), #  p-values of Fisher-exact test
-  n_cases = c(101, 196, 228),  # Sample sizes of cases = IRIS
-  n_controls = c(367, 367, 367)  # Sample sizes of LoewenKIDS
+  comparison = c("IRIS1 vs LoewenKIDS", "IRIS2 vs LoewenKIDS", "IRIS3 vs LoewenKIDS"),
+  OR = c(     2.0364, 3.7437, 0.8365),  # odds ratios of Fisher-exact test
+  p_value = c(0.2711, 0.0621, 0.7362),  #  p-values of Fisher-exact test
+  n_cases = c(   101,     95,     32),  # Sample sizes of cases = IRIS
+  n_controls = c(367,    367,    367)   # Sample sizes of LoewenKIDS
 )
 
 # Calculates Z-scores and standard errors from p-values
-data$Z <- qnorm(1 - data$p_value / 2)
-data$SE <- log(data$OR) / data$Z
+data$Z  <- qnorm(1 - data$p_value / 2)
+data$SE <- abs( log(data$OR) / data$Z )
 
 # run meta-analysis
 meta_result <- metagen(
@@ -26,11 +26,11 @@ meta_result <- metagen(
 # results
 summary(meta_result)
 
-png("forest_plot.png", width = 800, height = 600)
+png("forest_plot_single.png", width = 800, height = 600)
 forest(meta_result)
 dev.off()
 
 # Save the funnel plot as a PNG
-png("funnel_plot.png", width = 800, height = 600)
+png("funnel_plot_single.png", width = 800, height = 600)
 funnel(meta_result)
 dev.off()
